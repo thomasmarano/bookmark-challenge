d2f72e4d90fed46c044f2f968c9281444ff33871
thomasmarano <thomas.marano2@gmail.com>
2019-01-30 15:13:58 +0000

    Website displays titles (versus URL's)

--- app.rb
+++ app.rb
@@ -13,11 +13,12 @@ enable :sessions
 
     get '/bookmarks/new' do
       @bookmark = Bookmark.all
+      p @bookmark
       erb (:bookmarks)
     end
 
     post '/bookmark-saver' do
-      Bookmark.create(params[:website])
+      Bookmark.create(params[:website], params[:bookmark_name])
       p "Form data submitted to the /bookmark-saver route!"
       redirect '/bookmarks/new'
     end
